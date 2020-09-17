694ebb7b2a52adef981c3a449da60c2f7dd8f9b6
Trey McGarity <trey.mcgarity99@gmail.com>
2020-09-17 13:41:52 -0500

    some first pass or improved methods

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,11 +2,27 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+# using built-in is same as using set() logic but with a list()
+
+# def single_number(arr):
+#     # traverse through list
+#     for i in arr: 
+#         # if count of current value
+#         if arr.count(i) == 1:
+#             # return value
+#             return i
+# O(n^2)
+
 def single_number(arr):
-    # Your code here
-
-    pass
-
+    s = set()
+    for num in arr: # O(n)
+        if num in s: # O(1)
+            s.remove(num)   # O(1)
+        else:
+            s.add(num)  # O(1)
+    #only element in set is odd-element-out
+    return list(s)[0] # O(1) because will always have 1 element
+# O (n)
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
